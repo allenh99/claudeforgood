cc4274a38a211ce1571016aa35fa832c77a9032e
aleXyaO7 <alexyao0741@gmail.com>
2025-11-15 13:30:54 -0500

    chatbot and more files

---
+++ backend/chatbot.py
@@ -0,0 +1,19 @@
+import os
+from openai import OpenAI
+
+class Chatbot:
+    def __init__(self):
+        self.client = OpenAI()
+        self.model_version = os.getenv("OPENAI_MODEL")
+
+    def response(self, conversation, context='', temperature=0.7, max_tokens=100):
+        messages = [{"role": "system", "content": context}]
+        for role, message in conversation:
+            messages.append({"role": role, "content": message})
+        response = self.client.chat.completions.create(
+            model       = self.model_version,
+            messages    = messages,
+            temperature = temperature,
+            max_tokens  = max_tokens,
+        )
+        return response.choices[0].message.content.strip()
---
+++ test.py
@@ -0,0 +1,3 @@
+from backend.workflow import reset_data
+
+reset_data()
